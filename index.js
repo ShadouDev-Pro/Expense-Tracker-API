0ecc0f231d45d2092845fb63dba6f956e1613d4d
ShadouDev <shadoudev@proton.me>
2026-03-03 20:00:04 +0100

    Versión 0.1.0 (DEL y Update)

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(3000, () => {
 
 // Crear gasto
 app.post("/gastos", (req, res) => {
-  const { descripcion, monto } = req.body;
+  const { descripcion, monto, categoria } = req.body;
 
   // Validaciones
   if (!descripcion || descripcion.trim() === "") {
@@ -40,10 +40,17 @@ app.post("/gastos", (req, res) => {
     });
   }
 
+  if (!categoria || categoria.trim() === "") {
+  return res.status(400).json({
+    error: "La categoría es obligatoria"
+    });
+  }
+
   const nuevoGasto = {
     id: nextId++,
     descripcion,
     monto,
+    categoria,
     fecha: new Date()
   };
 
@@ -55,4 +62,88 @@ app.post("/gastos", (req, res) => {
 // Listar gastos
 app.get("/gastos", (req, res) => {
   res.json(gastos);
+});
+
+// Eliminar gasto
+app.delete("/gastos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  // Validar ID
+  if (isNaN(id)) {
+    return res.status(400).json({
+      error: "El ID debe ser un número"
+    });
+  }
+
+  // Buscar gasto
+  const index = gastos.findIndex(gasto => gasto.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({
+      error: "Gasto no encontrado"
+    });
+  }
+
+  // Eliminar gasto
+  gastos.splice(index, 1);
+
+  res.json({
+    mensaje: "Gasto eliminado correctamente"
+  });
+});
+
+// Actualizar gasto
+app.put("/gastos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { descripcion, monto, categoria } = req.body;
+
+  // Validar ID
+  if (isNaN(id)) {
+    return res.status(400).json({
+      error: "El ID debe ser un número"
+    });
+  }
+
+  // Buscar gasto
+  const gasto = gastos.find(g => g.id === id);
+
+  if (!gasto) {
+    return res.status(404).json({
+      error: "Gasto no encontrado"
+    });
+  }
+
+  // Validar descripción
+  if (!descripcion || descripcion.trim() === "") {
+    return res.status(400).json({
+      error: "La descripción es obligatoria"
+    });
+  }
+
+  // Validar monto
+  if (monto === undefined || typeof monto !== "number") {
+    return res.status(400).json({
+      error: "El monto debe ser un número"
+    });
+  }
+
+  if (monto <= 0) {
+    return res.status(400).json({
+      error: "El monto debe ser mayor a 0"
+    });
+  }
+
+  // Validar categoría
+  if (!categoria || categoria.trim() === "") {
+    return res.status(400).json({
+      error: "La categoría es obligatoria"
+    });
+  }
+
+  // Actualizar valores
+  gasto.descripcion = descripcion;
+  gasto.monto = monto;
+  gasto.categoria = categoria;
+
+  res.json(gasto);
 });
